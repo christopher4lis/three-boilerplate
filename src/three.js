061fbd286a3ae66c6b2961c3970ff93cfe9bbc54
christopher4lis <christopher4lis@gmail.com>
2017-09-16 18:03:03 -0400

    Initial commit. Webpack, ES6, Three, and ghetto HMR.

---
+++ src/three.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three';
+import OrbitControls from 'orbit-controls-es6';
+
+// Initial HMR Setup
+if (module.hot) {
+    module.hot.accept();
+
+    module.hot.dispose(() => {
+        document.querySelector('canvas').remove();
+        renderer.forceContextLoss();
+        renderer.context = null;
+        renderer.domElement = null;
+        renderer = null;
+        cancelAnimationFrame(animationId);
+        removeEventListener('resize', resize);
+    });
+}
+
+// Three Scene
+let scene, camera, renderer, animationId, controls;
+let geometry, material, mesh;
+
+function init() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+
+    controls = new OrbitControls(camera);
+
+    geometry = new THREE.BoxGeometry(200, 200, 200);
+    material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    for (let i = -5; i <= 5; i++) {
+        const geometry = new THREE.BoxGeometry(200, 200, 200);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+        mesh.position.x = i * 400;
+    }
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+    animationId = requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.04;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+}
+
+init();
+animate();
+
+// Event listeners
+function resize() {
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(innerWidth, innerHeight);
+}
+
+addEventListener('resize', resize);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: './src/three.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        publicPath: '/js/',
+        filename: 'three.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader!postcss-loader!sass-loader"
+            })
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), // Enable HMR
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin("../css/style.css"),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3001,
+            proxy: 'http://localhost:8080/',
+            files: [{
+                match: [
+                    '**/*.html'
+                ],
+                fn: function(event, file) {
+                    if (event === "change") {
+                        const bs = require('browser-sync').get('bs-webpack-plugin');
+                        bs.reload();
+                    }
+                }
+            }]
+        }, {
+            reload: false
+        }),
+    ],
+    devServer: {
+        hot: true, // Tell the dev-server we're using HMR
+        contentBase: path.resolve(__dirname, 'dist'),
+        publicPath: '/js/',
+    },
+    watch: true,
+    devtool: 'cheap-eval-source-map'
+};
