c11244ffea7976e507113912fab53a1f2494330e
christopher4lis <christopher4lis@gmail.com>
2017-11-24 22:12:58 -0500

    Reformat js, update eslint config, add shader placeholder with GLSL.

--- src/three.js
+++ src/three.js
@@ -1,71 +1,85 @@
-import * as THREE from 'three';
-import OrbitControls from 'orbit-controls-es6';
+import * as THREE from 'three'
+import OrbitControls from 'orbit-controls-es6'
+
+import vert from './shaders/shader.vert'
+import frag from './shaders/shader.frag'
 
 // Initial HMR Setup
 if (module.hot) {
-    module.hot.accept();
+    module.hot.accept()
 
     module.hot.dispose(() => {
-        document.querySelector('canvas').remove();
-        renderer.forceContextLoss();
-        renderer.context = null;
-        renderer.domElement = null;
-        renderer = null;
-        cancelAnimationFrame(animationId);
-        removeEventListener('resize', resize);
-    });
+        document.querySelector('canvas').remove()
+        renderer.forceContextLoss()
+        renderer.context = null
+        renderer.domElement = null
+        renderer = null
+        cancelAnimationFrame(animationId)
+        removeEventListener('resize', resize)
+    })
 }
 
 // Three Scene
-let scene, camera, renderer, animationId, controls;
-let geometry, material, mesh;
+let scene, camera, renderer, animationId, controls
+let geometry, material, mesh
 
 function init() {
-    scene = new THREE.Scene();
+    scene = new THREE.Scene()
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000
+    )
+    camera.position.z = 1000
 
-    controls = new OrbitControls(camera);
+    controls = new OrbitControls(camera)
 
-    geometry = new THREE.BoxGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+    geometry = new THREE.BoxGeometry(200, 200, 200)
+    material = new THREE.RawShaderMaterial({
+        vertexShader: vert,
+        fragmentShader: frag
+    })
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
 
     for (let i = -5; i <= 5; i++) {
-        const geometry = new THREE.BoxGeometry(200, 200, 200);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+        const geometry = new THREE.BoxGeometry(200, 200, 200)
+        const material = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            wireframe: true
+        })
 
-        const mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
-        mesh.position.x = i * 400;
+        const mesh = new THREE.Mesh(geometry, material)
+        scene.add(mesh)
+        mesh.position.x = i * 400
     }
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
 
-    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement)
 }
 
 function animate() {
-    animationId = requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate)
 
-    mesh.rotation.x += 0.04;
-    mesh.rotation.y += 0.02;
+    mesh.rotation.x += 0.04
+    mesh.rotation.y += 0.02
 
-    renderer.render(scene, camera);
+    renderer.render(scene, camera)
 }
 
-init();
-animate();
+init()
+animate()
 
 // Event listeners
 function resize() {
-    camera.aspect = innerWidth / innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = innerWidth / innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(innerWidth, innerHeight)
 }
 
-addEventListener('resize', resize);
+addEventListener('resize', resize)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
-const path = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const webpack = require('webpack')
+const path = require('path')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
     entry: './src/three.js',
@@ -10,44 +10,61 @@ module.exports = {
         filename: 'three.bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
                 }
+            },
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'glslify-loader',
+                exclude: /node_modules/
             }
-        }]
+        ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(), // Enable HMR
         new webpack.NamedModulesPlugin(),
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3001,
-            proxy: 'http://localhost:8080/',
-            files: [{
-                match: [
-                    '**/*.html'
-                ],
-                fn: (event) => {
-                    if (event === 'change') {
-                        const bs = require('browser-sync').get('bs-webpack-plugin');
-                        bs.reload();
+        new BrowserSyncPlugin(
+            {
+                host: 'localhost',
+                port: 3001,
+                proxy: 'http://localhost:8080/',
+                files: [
+                    {
+                        match: ['**/*.html'],
+                        fn: event => {
+                            if (event === 'change') {
+                                const bs = require('browser-sync').get(
+                                    'bs-webpack-plugin'
+                                )
+                                bs.reload()
+                            }
+                        }
                     }
-                }
-            }]
-        }, {
-            reload: false
-        }),
+                ]
+            },
+            {
+                reload: false
+            }
+        )
     ],
     devServer: {
         hot: true, // Tell the dev-server we're using HMR
         contentBase: path.resolve(__dirname, 'dist'),
-        publicPath: '/js/',
+        publicPath: '/js/'
     },
     watch: true,
     devtool: 'cheap-eval-source-map'
-};
+}
