e41443783e33a9c5c1e6431e5b15f4cadd598b92
christopher4lis <christopher4lis@gmail.com>
2020-01-11 12:51:30 -0500

    chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,66 @@ const path = require('path')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
-    entry: './src/three.js',
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        publicPath: '/js/',
-        filename: 'three.bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            },
-            {
-                test: /\.(glsl|frag|vert)$/,
-                loader: 'raw-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(glsl|frag|vert)$/,
-                loader: 'glslify-loader',
-                exclude: /node_modules/
+  entry: './src/three.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    publicPath: '/js/',
+    filename: 'three.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // Enable HMR
+    new webpack.NamedModulesPlugin(),
+    new BrowserSyncPlugin(
+      {
+        host: 'localhost',
+        port: 3001,
+        proxy: 'http://localhost:8080/',
+        files: [
+          {
+            match: ['**/*.html'],
+            fn: event => {
+              if (event === 'change') {
+                const bs = require('browser-sync').get('bs-webpack-plugin')
+                bs.reload()
+              }
             }
+          }
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(), // Enable HMR
-        new webpack.NamedModulesPlugin(),
-        new BrowserSyncPlugin(
-            {
-                host: 'localhost',
-                port: 3001,
-                proxy: 'http://localhost:8080/',
-                files: [
-                    {
-                        match: ['**/*.html'],
-                        fn: event => {
-                            if (event === 'change') {
-                                const bs = require('browser-sync').get(
-                                    'bs-webpack-plugin'
-                                )
-                                bs.reload()
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                reload: false
-            }
-        )
-    ],
-    devServer: {
-        hot: true, // Tell the dev-server we're using HMR
-        contentBase: path.resolve(__dirname, 'dist'),
-        publicPath: '/js/'
-    },
-    watch: true,
-    devtool: 'cheap-eval-source-map'
+      },
+      {
+        reload: false
+      }
+    )
+  ],
+  devServer: {
+    hot: true, // Tell the dev-server we're using HMR
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/js/'
+  },
+  watch: true,
+  devtool: 'cheap-eval-source-map'
 }
